eac4d286f10956fcd992f85e9d7c452faa93b2c3
Ivanova Anna <annprokudina@gmail.com>
2021-11-13 17:04:01 +0200

    Added UITableView

---
+++ CatBook/RatingTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RatingTableViewController.swift
+//  CatBook
+//
+//  Created by Anna Ivanova on 13.11.2021.
+//
+
+import UIKit
+
+class RatingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ratingOfCats", for: indexPath)
+
+
+        return cell
+    }
+
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+
+}
