9e38302a6b399157441b59843853d540108552ae
Ivanova Anna <annprokudina@gmail.com>
2021-11-13 17:55:14 +0200

    Added cat page

---
+++ CatBook/CatPageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CatPageViewController.swift
+//  CatBook
+//
+//  Created by Anna Ivanova on 13.11.2021.
+//
+
+import UIKit
+
+class CatPageViewController: UIViewController {
+
+    @IBOutlet var imageCat: UIImageView!
+    @IBOutlet var nameCatLable: UILabel!
+    @IBOutlet var descriptionСatLable: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
